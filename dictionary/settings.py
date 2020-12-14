968fddbdc60532ba57e23f7b81623364f8573aef
Roman Slesarev <rslesarev@distillery.com>
2020-12-14 16:43:25 +0300

    fix build

--- dictionary/settings.py
+++ dictionary/settings.py
@@ -129,14 +129,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'static')
+  os.path.join(BASE_DIR, 'vocabulary')
 ]
 
 SITE_ID = 1
